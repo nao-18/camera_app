841cd2df564c66fc36220972a7968606a4501def
nao18 <justdo.nao01@gmail.com>
2021-05-03 19:36:13 +0900

    フルサイズの画像を端末へ保存

--- app/src/main/java/com/example/mycamera/MainActivity.kt
+++ app/src/main/java/com/example/mycamera/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.mycamera
 
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import com.example.mycamera.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     val REQUEST_PREVIEW = 1
+    val REQUEST_PICTURE = 2
+
+    lateinit var currentPhotoUri: Uri
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,13 +47,37 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    private fun takePicture() {}
+    private fun takePicture() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+            intent.resolveActivity(packageManager)?.also {
+                val time: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+                val values = ContentValues().apply {
+                    put(MediaStore.Images.Media.DISPLAY_NAME, "$(time)_.jpg")
+                    put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                }
+                val collection = MediaStore.Images.Media.getContentUri("external")
+                val photoUri = contentResolver.insert(collection, values)
+                photoUri?.let {
+                    currentPhotoUri = it
+                }
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                startActivityForResult(intent, REQUEST_PICTURE)
+            }
+        }
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_PREVIEW && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.get("data") as Bitmap
             binding.imageView.setImageBitmap(imageBitmap)
+        } else if (requestCode == REQUEST_PICTURE) {
+            when (resultCode) {
+                RESULT_OK -> {}
+                else -> {
+                    contentResolver.delete(currentPhotoUri, null, null)
+                }
+            }
         }
     }
 }
